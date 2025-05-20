d0b1ff7153edbfa96af6e1d8c1686a588696eed7
SelGo <tsampakiourispetros@gmail.com>
2025-05-20 13:16:06 +0300

    Added profile and profile contact sections.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatGridListModule],
+  imports: [RouterOutlet, MatGridListModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
