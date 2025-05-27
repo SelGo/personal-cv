561830fe2aeb9a0423c8c1546cad9ceedf3fa711
SelGo <tsampakiourispetros@gmail.com>
2025-05-27 12:53:59 +0300

    Updated the main layout. First version of CV.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatGridListModule, MatIconModule],
+  imports: [RouterOutlet, MatGridListModule, MatIconModule, MatChipsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
