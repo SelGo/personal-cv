51c9f17dff9d9cc5c5a528bd730f8714457bb751
SelGo <tsampakiourispetros@gmail.com>
2025-05-30 12:37:07 +0300

    General Fixes and Changes. Added a PDF export button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonModule } from '@angular/material/button';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatGridListModule, MatIconModule, MatChipsModule],
+  imports: [RouterOutlet, MatGridListModule, MatIconModule, MatChipsModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'personal-cv';
+
+  @ViewChild('containerForExport', { static: false }) containerForExport!: ElementRef;
+
+  exportToPdf() {
+    const content = this.containerForExport.nativeElement;
+    const options = {
+      scale: 2, // Higher scale for better quality
+      useCORS: true // If you have external images
+    };
+
+    html2canvas(content, options).then(canvas => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF({
+        orientation: 'p', // portrait
+        unit: 'mm',
+        format: 'a4'
+      });
+
+      const imgProps = pdf.getImageProperties(imgData);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      let position = 0;
+      let heightLeft = pdfHeight;
+
+      pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, pdfHeight);
+      heightLeft -= pdf.internal.pageSize.getHeight();
+
+      while (heightLeft >= 0) {
+        position = heightLeft - pdfHeight;
+        pdf.addPage();
+        pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, pdfHeight);
+        heightLeft -= pdf.internal.pageSize.getHeight();
+      }
+      pdf.save('peter-tsampakiouris-cv.pdf');
+    });
+  }
 }
